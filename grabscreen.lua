88a720d428abf3866ce1075a3207f9041c94c27d
Philipp Kutin <philipp.kutin@gmail.com>
2020-01-31 22:03:09 +0100

    grabscreen.lua: implement "send big tile in the middle" as it was intended.

--- grabscreen.lua
+++ grabscreen.lua
@@ -220,13 +220,13 @@ local Sampler = class
                     destTileChanged = destTileChanged or (fbSampleBuf[si] ~= scSampleBuf[si])
                 end
 
-                local lastCoords = destTileCoords[#destTileCoords]
-
-                if (lastCoords ~= nil and lastCoords.y == y and lastCoords.x == x - 2) then
-                    destTileCoords[#destTileCoords + 1] = { x=x-1, y=y }
-                end
-
                 if (destTileChanged) then
+                    local lastCoords = destTileCoords[#destTileCoords]
+
+                    if (lastCoords ~= nil and lastCoords.y == y and lastCoords.x == x - 2) then
+                        destTileCoords[#destTileCoords + 1] = { x=x-1, y=y }
+                    end
+
                     destTileCoords[#destTileCoords + 1] = { x=x, y=y }
                 end
             end
