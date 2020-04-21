5f38685c9ec6545f8790d2167fbc6d116a4084cb
Philipp Kutin <philipp.kutin@gmail.com>
2020-04-21 16:53:58 +0200

    mklayout.lua: allow multiple positional arguments (layouts)

--- mklayout.lua
+++ mklayout.lua
@@ -8,6 +8,7 @@ local kb_layout_util = require("kb_layout_util")
 
 local assert = assert
 local error = error
+local ipairs = ipairs
 local loadstring = loadstring
 local print = print
 local type = type
@@ -16,37 +17,39 @@ local arg = arg
 
 ----------
 
-local layout = arg[1]
-
-if (layout == nil) then
-    print("Usage: mklayout.lua <layout>")
+if (arg[1] == nil) then
+    print("Usage: mklayout.lua <layout1> [<layout2> ...]")
     os.exit(1)
 end
 
-local layoutAsLua, msg = xkb_symbols_reader.as_lua(layout)
-if (layoutAsLua == nil) then
-    io.stderr:write(("ERROR: %s\n"):format(msg))
-    os.exit(1)
-end
-assert(type(layoutAsLua) == "string")
+for _, layout in ipairs(arg) do
+    print("["..layout.."]")
 
--- Validate generated Lua code.
-do
-    local tab, msg = kb_layout_util.get_table(
+    local layoutAsLua, msg = xkb_symbols_reader.as_lua(layout)
+    if (layoutAsLua == nil) then
+        io.stderr:write(("ERROR: %s\n"):format(msg))
+        os.exit(1)
+    end
+    assert(type(layoutAsLua) == "string")
+
+    -- Validate generated Lua code.
+    do
+        local tab, msg = kb_layout_util.get_table(
             -- NOTE: potentially more than one return value:
             loadstring(layoutAsLua, "<generated>"))
-    if (tab == nil) then
-        error("INTERNAL ERROR: "..msg)
+        if (tab == nil) then
+            error("INTERNAL ERROR: "..msg)
+        end
     end
+
+    local layoutLuaFileName = ("./layouts/%s"):format(layout)
+
+    local f, msg = io.open(layoutLuaFileName, 'w')
+    if (f == nil) then
+        io.stderr:write("ERROR: Failed opening for writing "..msg..'\n')
+        os.exit(1)
+    end
+
+    f:write(layoutAsLua)
+    f:close()
 end
-
-local layoutLuaFileName = ("./layouts/%s"):format(layout)
-
-local f, msg = io.open(layoutLuaFileName, 'w')
-if (f == nil) then
-    io.stderr:write("ERROR: Failed opening for writing "..msg..'\n')
-    os.exit(1)
-end
-
-f:write(layoutAsLua)
-f:close()
