helixhorned/ljremarkable

commit 91fbfe171d305d78bec33c8d250ff3a6b80f7933

grabscreen.lua: fix access of nil 'data' local when tapping while dragging.
To be confronted with assert "more touch release than press events".

grabscreen.lua
@@ -1120,6 +1120,7 @@ local InputState = class
             local delta = ((events[0].value >= 0) and 1 or -1)
 
             self.pressedCount = oldPressedCount + delta
+            -- FIXME: frequently, but not always, fails on tap while dragging.
             assert(self.pressedCount >= 0, "more touch release than press events")
 
             local havePressed = (oldPressedCount == 0 and delta == 1)
@@ -1178,7 +1179,6 @@ local InputState = class
                     local MemberTab = { [MTC.POSX]='x', [MTC.POSY]='y' }
                     local m = MemberTab[events[i].code]
                     if (m == nil) then
-                        self:reset()
                         return nil
                     end
                     local newValue = events[i].value
@@ -1193,9 +1193,13 @@ local InputState = class
                     local MaxDeviation = MaxSingleClickDeviation * MultiTouchScreenUnitRatio
                     for i = 0, eventCount - 1 do
                         local delta = updateNewPos(i)
+                        if (delta == nil) then
+                            self:reset()
+                            break
+                        end
                         -- Allow a small deviation from the initially tapped point for
                         -- "single click". If it is exceeded, consider it as starting...
-                        if (delta ~= nil and math.abs(delta) > MaxDeviation) then
+                        if (math.abs(delta) > MaxDeviation) then
                             self.ourEventType = OurEventType.Drag  -- <- ...this.
                             assert(self.lastFirstPressedTime ~= math.huge)
                             local msSinceTap = currentTimeMs() - self.lastFirstPressedTime
@@ -1211,9 +1215,15 @@ local InputState = class
                     local TriangRegionCheckXOffset = SingleWheelRollDistance * MultiTouchScreenUnitRatio
 
                     for i = vDragStartEvIdx, eventCount - 1 do
+                        if (updateNewPos(i) == nil) then
+                            self:reset()
+                            break
+                        end
+
                         local data = self.ourData
                         local ody = data.ny - data.y
-                        if (updateNewPos(i) ~= nil and self.onlyVerticalDrag) then
+
+                        if (self.onlyVerticalDrag) then
                             local dx, dy = data.nx - data.x, data.ny - data.y
                             -- After the initial single click tolerance, vertical dragging
                             -- has to proceed (1) up or down consistently, and (2) in an

commit 2477d253859b3fc8b4c051716d4455c24c74d462

grabscreen.lua: fix check for consistent up/down dragging.
Broken in commit 91fbfe17.

grabscreen.lua
@@ -1417,6 +1417,11 @@ local InputState = class
             return self:reset()
         end
 
+        local dysPreLoop = {
+            [0] = self.ourData[0].ny - self.ourData[0].y,
+            [1] = self.ourData[1] and (self.ourData[1].ny - self.ourData[1].y) or 0,
+        }
+
         for i = startEventIdx, eventCount - 1 do
             local ev = events[i]
             assert(ev.type == EV.ABS, "unexpected event type")
@@ -1438,7 +1443,8 @@ local InputState = class
                 return
             elseif (self.onlyVerticalDrag) then
                 local data = self.ourData[self.slot]
-                local ody = data.ny - data.y
+                local ody = dysPreLoop[self.slot]
+                assert(ody ~= nil)
                 local dx, dy = data.nx - data.x, data.ny - data.y
                 -- After the initial single click tolerance, vertical dragging
                 -- has to proceed (1) up or down consistently, and (2) in an