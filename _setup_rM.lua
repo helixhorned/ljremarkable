f84bc8e6f231aadc6a23f41a40ea023f6ff6c8fe
Philipp Kutin <philipp.kutin@gmail.com>
2020-08-10 16:00:52 +0200

    _setup_rM.lua: make checklua.sh-clean.
    (My custom script, not yet published.)

--- _setup_rM.lua
+++ _setup_rM.lua
@@ -1,35 +1,57 @@
 #!/bin/false
 
 -- Debugging setup, to be loaded as Lua module on the LuaJIT command line on the rM.
--- NOTE: hence, deliberately assigning in global environment.
 
-ffi=require'ffi'
-FB=require'framebuffer'
-fb=FB.FrameBuffer(0, true)
-map=fb:getMapping()
+-- NOTE: will assign to global environment at the end.
+local _G = _G
 
-RM=require'remarkable'
-rM=RM.Remarkable()
+local ffi=require'ffi'
+local FB=require'framebuffer'
+local fb=FB.FrameBuffer(0, true)
+local map=fb:getMapping()
+
+local RM=require'remarkable'
+local rM=RM.Remarkable()
+
+local math = require("math")
+local os = require("os")
 
 local assert = assert
+local require = require
 
-function rgb(r, g, b)
-    assert(r>=0 and r <= 31)
-    assert(g>=0 and g <= 63)
-    assert(b>=0 and b <= 31)
+local arg = arg
+
+----------
+
+local MaxColorComponentVal = { r=31, g=63, b=31 }
+
+local function rgb(r, g, b)
+    assert(r>=0 and r <= MaxColorComponentVal.r)
+    assert(g>=0 and g <= MaxColorComponentVal.g)
+    assert(b>=0 and b <= MaxColorComponentVal.b)
 
     return b + 32*g + 32*64*r
 end
 
 -- Fill+update.
-function fup(x, y, w, h, val)
+local function fup(x, y, w, h, val)
     map:fill(x, y, w, h, val)
     rM:requestRefresh(RM.xywh(x,y,w,h), 123)
 end
 
-input=require'input'
-MTC=input.MultiTouchCode
-evd=input.EventDevice(1)
+_G.ffi = ffi
+_G.FB = FB
+_G.fb = fb
+_G.map = map
+_G.RM = RM
+_G.rM = rM
+
+_G.rgb = rgb
+_G.fup = fup
+
+_G.input = require'input'
+_G.MTC = _G.input.MultiTouchCode
+_G.evd = _G.input.EventDevice(1)
 
 if (arg[1] == '-Q' and #arg == 1) then
     -- Making .app.lua
