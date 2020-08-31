9dd7b0cc873476a337e6d21fe7919e48726b73b9
Philipp Kutin <philipp.kutin@gmail.com>
2020-08-31 17:33:08 +0200

    src: in interpose demo, detect whether a PDF page is drawn non-zoomed.
    Within a 0.1 percent tolerance.

--- src/pdfium_interpose_info.c
+++ src/pdfium_interpose_info.c
@@ -3,8 +3,31 @@
 #include "pdfium/fpdfview.h"
 #include "interpose/include/interpose.h"
 
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
+
+#define SCREEN_WIDTH 1404
+#define SCREEN_HEIGHT 1872
+
+static
+double GetScale(double width, double height) {
+    return (height > 0.0 && width > 0.0) ?
+        fmin(SCREEN_HEIGHT/height, SCREEN_WIDTH/width) :
+        0.0;
+}
+
+static
+double GetScaleForPage(FPDF_PAGE page) {
+    return GetScale(FPDF_GetPageWidth(page), FPDF_GetPageHeight(page));
+}
+
+static
+double Equals(double val, double refVal, double maxRelativeDeviation) {
+    return (isfinite(refVal) && refVal != 0.0 &&
+            fabs((val / refVal) - 1.0) <= maxRelativeDeviation);
+}
 
 INTERPOSE_C(FPDF_DOCUMENT, FPDF_LoadDocument,
             (FPDF_STRING file_path, FPDF_BYTESTRING password),
@@ -23,7 +46,11 @@ INTERPOSE_C(FPDF_PAGE, FPDF_LoadPage, (FPDF_DOCUMENT document, int page_index),
             (document, page_index)) {
     FPDF_PAGE page = Real__FPDF_LoadPage(document, page_index);
     printf("INFO: LoadPage: doc=%p, i=%d -> page=%p\n", (void *)document, page_index, (void *)page);
-    printf("      dimensions => %f x %f\n", FPDF_GetPageWidth(page), FPDF_GetPageHeight(page));
+    const double width = FPDF_GetPageWidth(page);
+    const double height = FPDF_GetPageHeight(page);
+    printf("      dimensions => %f x %f\n"
+           "      scale => %f\n",
+           width, height, GetScale(width, height));
     return page;
 }
 
@@ -57,11 +84,18 @@ INTERPOSE_C_VOID(FPDF_RenderPageBitmapWithMatrix,
 
     const FS_MATRIX *m = matrix ? matrix : &zeroMatrix;
     const FS_RECTF *c = clipping ? clipping : &zeroClip;
+
+    const double scale = GetScaleForPage(page);
+    const bool isNoZoom = (Equals(m->a, scale, 0.001) && Equals(m->d, scale, 0.001) &&
+                           m->b == 0.0 && m->c == 0.0 &&
+                           m->e == 0.0 && m->f == 0.0);
+
     printf("INFO: Render: m = [%f %f 0;  bitmap=%p, flags=0x%x\n"
-           "                   %f %f 0;\n"
+           "                   %f %f 0;%s\n"
            "                   %f %f 1]\n"
            "              clip = (%f, %f)--(%f, %f)\n",
-           m->a, m->b, (void *)bitmap, flags, m->c, m->d, m->e, m->f,
+           m->a, m->b, (void *)bitmap, flags, m->c, m->d,
+           isNoZoom ? "  [NO ZOOM]" : "", m->e, m->f,
            c->left, c->top, c->right, c->bottom);
 
     const Millisecs startMs = curTimeMs();
